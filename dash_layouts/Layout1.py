fc476935c3b2c9a3bcfb52b42a48b339719ec01e
mpkuse <mpkuse@connect.ust.hk>
2018-04-29 15:13:37 +0800

    basic dash app with revenue and gross income

--- dash_layouts/Layout1.py
+++ dash_layouts/Layout1.py
@@ -16,7 +16,7 @@ class Layout1:
         """ Returns a Dash layout """
 
         r = Retriver('https://localhost:5000')
-        self.r = r 
+        self.r = r
         url = '/industryInfo/HK/'
         url = '/industryInfo/all/Automotive:Agriculture/all'
         url = '/industryInfo/all/all/all'
@@ -27,12 +27,36 @@ class Layout1:
         for industry in data_dict.keys():
             for sector in data_dict[industry].keys():
                 for ticker in data_dict[industry][sector].keys():
-                    label = '%s %s' %(ticker, data_dict[industry][sector][ticker]['companyName'])
+                    label = '%s %s' %(ticker, data_dict[industry][sector][ticker]['companyName'] )
                     options.append( {'label': label, 'value': ticker} )
         dropdown = dcc.Dropdown( options=options, multi=False, id='my-dropdown' )
+
+
+        graph = dcc.Graph(
+        id='basic-interactions',
+        figure={
+            'data': [
+                {
+                    'x': [1, 2, 3, 4],
+                    'y': [4, 1, 3, 5],
+                #    'text': ['a', 'b', 'c', 'd'],
+                #    'customdata': ['c.a', 'c.b', 'c.c', 'c.d'],
+                #    'name': 'Trace 1',
+                #    'mode': 'markers',
+                #    'marker': {'size': 12}
+                }
+                    ]
+                }
+                )
+
+        description_div = html.Div( [html.B("Description"), html.P(id='description') ] )
+        accounting_currency_div = html.Div( [html.B("Accounting Currency"), html.P(id='accounting_currency') ] )
 
         return html.Div( [html.H1("Welcome Dash!"),\
                           html.P("This is dash react"),\
                           html.Div( dropdown ),\
-                          html.Div( id='output' )
+                          description_div,\
+                          accounting_currency_div,\
+                          html.Div( id='debug' ),\
+                          graph
                           ])
--- dash_layouts/dev_app.py
+++ dash_layouts/dev_app.py
@@ -6,7 +6,8 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-
+import numpy as np
+import json
 
 dash_app = dash.Dash( __name__ ) #can set a static folder here (separate for it for images etc)
 
@@ -16,10 +17,67 @@ from Layout1 import Layout1
 lay = Layout1(dash_app)
 dash_app.layout = lay.render()
 
-@dash_app.callback( Output('output', 'children'), [Input( 'my-dropdown', 'value' ) ] )
+@dash_app.callback( Output('description', 'children'), [Input( 'my-dropdown', 'value' ) ] )
 def update_info( input_value ):
     # return 'you entered : '+ str(input_value)
-    return lay.r.geturl_as_raw( '/tickerInfo/%s/description' %(input_value) )
+    if input_value is not None:
+        return lay.r.geturl_as_dict( '/tickerInfo/%s/description' %(input_value) )[input_value]
+
+
+@dash_app.callback( Output('accounting_currency', 'children'), [Input( 'my-dropdown', 'value' ) ] )
+def update_info( input_value ):
+    # return 'you entered : '+ str(input_value)
+    if input_value is not None:
+        d = lay.r.geturl_as_dict( '/tickerInfo/%s/accountingCurrency/2016' %(input_value) )[input_value]['2016']['_FISCAL_NOTE_']
+        return str(d)
+
+
+@dash_app.callback( Output('basic-interactions', 'figure'), [Input( 'my-dropdown', 'value' ) ] )
+# @dash_app.callback( Output('debug', 'children'), [Input( 'my-dropdown', 'value' ) ] )
+def update_fig( input_value ):
+    if input_value is None :
+        return
+
+    # Get Financial statements data
+    #https://35.194.163.228:5000
+    data_frame = lay.r.geturl_as_dict( '/accountingStatements/%s/is/all/Sales_Revenue:Gross%%20Income' %(input_value) )
+    # return str( data_frame )
+
+    x = []
+    revenue = []
+    gross_income = []
+    list_of_tuples = []
+    for ticker in data_frame:
+        for year in data_frame[ticker]:
+            try:
+                year_int = int(year)
+                if year_int > 2100:
+                    continue
+            except:
+                continue
+
+            _year = year_int
+            _revenue = data_frame[ticker][year]['Sales/Revenue']
+            _gross_income = data_frame[ticker][year]['Gross Income']
+            x.append( _year )
+            revenue.append( _revenue )
+            gross_income.append( _gross_income )
+            list_of_tuples.append( (_year, _revenue, _gross_income) )
+
+
+    list_of_tuples.sort( key=lambda tup: tup[0] )
+    li = list_of_tuples
+
+    # return '{"data": [{"y": [1, 2, 3], "x": [1, 2, 3]}]}'
+    data = []
+    # data.append( { 'x': x, 'y': revenue, 'name':'revenue' } )
+    # data.append( { 'x': x, 'y': gross_income, 'name':'gross income' } )
+    data.append( { 'x': [ l[0] for l in li ], 'y': [ l[1] for l in li ], 'name':'revenue' } )
+    data.append( { 'x': [ l[0] for l in li ], 'y': [ l[2] for l in li ], 'name':'gross income' } )
+
+    figure = { 'data': data }
+
+    return figure
 
 
 
